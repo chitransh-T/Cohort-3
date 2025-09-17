3be7fef4ea69308525b4555ce31743bf6388bfea
Your Name <chitranshtiwari78@gmail.com>
2025-09-17 15:43:11 +0530

    understanding DOM by creating todo list

---
+++ week-3/week-3-1/script.js
@@ -0,0 +1,18 @@
+function deletetodo(index){
+const element = document.getElementById(index);
+element.parentNode.removeChild(element)
+
+}
+    let ctr = 1;
+function addTodo(){
+
+    const element = document.querySelector("input")
+    const value = element.value
+    const divEl = document.createElement("div");
+    divEl.setAttribute('id' , ctr)
+  
+    divEl.innerHTML ="<div>" + ctr +'. ' + value +'</div> <button onClick="deletetodo(' + ctr + ')">delete</delete>'
+    const parenetEl =   document.querySelector("body");
+    parenetEl.appendChild(divEl);   
+      ctr = ctr + 1;
+}
