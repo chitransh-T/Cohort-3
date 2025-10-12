0bfb3573017d5c6a282982ed167a167bb2f0b530
Your Name <chitranshtiwari78@gmail.com>
2025-10-12 22:15:56 +0530

    http auth

---
+++ week-6/week-6-1/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+
+
+app.use(express.json());
+
+const users = [];
+
+
+app.post("/signup",function(req,res){
+   
+    const name = req.body.username;
+    const password = req.body.password;
+
+    users.push({
+        name: name,
+        password:password
+    })
+
+    res.status(200).json({
+        msg:"you are registered"
+    })
+})
+
+app.post("/signin",function(req,res){
+     const name = req.body.username;
+     const password = req.body.password;
+
+     let founduser = null;
+   
+
+})
+
+
+app.listen(3000);
